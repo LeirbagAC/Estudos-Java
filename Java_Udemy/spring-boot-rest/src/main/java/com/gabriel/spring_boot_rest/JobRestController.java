fa87bae022443ea3f69ea09aaca74cc2768b286b
LeirbagAC <albuquerqueg373@gmail.com>
2026-02-26 20:27:56 -0300

    Fazendo a conexão com o banco no JobApp

--- Java_Udemy/spring-boot-rest/src/main/java/com/gabriel/spring_boot_rest/JobRestController.java
+++ Java_Udemy/spring-boot-rest/src/main/java/com/gabriel/spring_boot_rest/JobRestController.java
@@ -28,6 +28,20 @@ public class JobRestController {
         return service.getJob(postId);
     }
 
+    /*
+    No design de APIs REST, usamos variáveis de caminho ({id}) para identificar um recurso único (ex: /jobPost/5).
+    Para buscas e filtros, o padrão global é usar parâmetros de consulta (Query Params), representados pelo sinal de interrogação na URL (ex: /jobPosts/search?keyword=java).
+    O motivo? Palavras-chave podem conter espaços ou caracteres especiais (ex: "C++ developer"), e isso quebra URLs baseadas em Path.*/
+//    @GetMapping("JobPost/keyword/{keyword}")
+//    public List<JobPost> search(@PathVariable String keyword) {
+//        return service.search(keyword);
+//    }
+
+    @GetMapping("JobPost/search")
+    public List<JobPost> search(@RequestParam String keyword) {
+        return service.search(keyword);
+    }
+
     @PostMapping("/JobPost") //O ResponseEntity para retornar 'Feedback' Visual (201 Created) e confirmação dos dados
     public ResponseEntity<JobPost> addJob(@RequestBody JobPost jobPost) {
         service.addJob(jobPost);
@@ -35,17 +49,29 @@ public class JobRestController {
                 .body(jobPost);
     }
 
-    @PutMapping("/JobPost/{postId}")
-    public ResponseEntity<JobPost> changeJob(@PathVariable("postId") int postId, @RequestBody JobPost jobPost) {
-        service.changeJob(jobPost, postId);
-        return ResponseEntity.status(HttpStatus.OK)
-                .body(jobPost);
-
+    @PutMapping("jobPost")
+    public JobPost updateJob(@RequestBody JobPost jobPost) {
+        service.updateJob(jobPost);
+        return service.getJob(jobPost.getPostId());
     }
+
+//    @PutMapping("/JobPost/{postId}")
+//    public ResponseEntity<JobPost> changeJob(@PathVariable("postId") int postId, @RequestBody JobPost jobPost) {
+//        service.changeJob(jobPost, postId);
+//        return ResponseEntity.status(HttpStatus.OK)
+//                .body(jobPost);
+//
+//    }
 
     @DeleteMapping("/JobPost/{postId}")
     public void deleteJob(@PathVariable int postId) {
         service.deleteJob(postId);
+    }
+
+    @GetMapping("/load")
+    public String loadData() {
+        service.load();
+        return "success";
     }
 
 }
